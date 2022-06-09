e478b504e2018252a35cbaeac746a9b372f47dff
Lethiers <fabien.tarne@gmail.com>
2022-06-09 16:07:42 +0200

    ajout page acceuil et autres

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,6 +6,8 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 use App\Entity\Ingredient;
+use App\Entity\Pizza;
+use App\Entity\Drink;
 use Faker;
 
 class AppFixtures extends Fixture
@@ -16,6 +18,11 @@ class AppFixtures extends Fixture
         $faker = Faker\Factory::create();
         //Tableau vide qui va stocker les utilisateurs que l’on génère
         $ingredients = [];
+        //Tableau vide qui va stocker les utilisateurs que l’on génère
+        $pizzas = [];
+        //Tableau vide qui va stocker les utilisateurs que l’on génère
+        $drinks = [];
+
         // boucle qui va integrer des ingredients
         for ($i=0; $i <50 ; $i++) { 
             $ingredient = new Ingredient();
@@ -26,6 +33,35 @@ class AppFixtures extends Fixture
             $ingredient->setQtxIngredient($faker->numberBetween(0, 10));
             $manager->persist($ingredient);
             $ingredients[]= $ingredient;
+
+        }
+
+        // boucle qui va integrer des ingredients
+        for ($i=0; $i <50 ; $i++) { 
+            $pizza = new Pizza();
+            // fausse données
+            $pizza->setNamePizza($faker->lexify());
+            $pizza->setDescPizza($faker->paragraph(2));
+            $pizza->setImgPizza('https://ehonline.eu/wp-content/uploads/2019/07/prix-de-revient-d-une-pizza-ingredients.jpg');
+            $pizza->setPricePizza($faker->numberBetween(0, 10));
+ 
+            $manager->persist($pizza);
+            $pizzas[]= $pizza;
+
+        }
+
+
+        // boucle qui va integrer des ingredients
+        for ($i=0; $i <50 ; $i++) { 
+            $drink = new Drink();
+            // fausse données
+            $drink->setNameDrink($faker->lexify());
+            $drink->setDescDrink($faker->paragraph(2));
+            $drink->setImgDrink('https://ehonline.eu/wp-content/uploads/2019/07/prix-de-revient-d-une-pizza-ingredients.jpg');
+            $drink->setPriceDrink($faker->numberBetween(0, 10));
+ 
+            $manager->persist($drink);
+            $drinks[]= $drink;
 
         }
 
