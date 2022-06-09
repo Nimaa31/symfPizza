7b288ee4973392be3b8c4eb9533a6bd6dc853769
Leef <mehdi.diouane31@gmail.com>
2022-06-09 10:29:21 +0200

    form inscription

---
+++ src/Controller/InscriptionFormController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\User;
+use App\Form\InscriptionType;
+use Doctrine\ORM\EntityManagerInterface;
+
+
+class InscriptionFormController extends AbstractController
+{
+    #[Route('/register', name: 'app_inscription')]
+    public function index(Request $request, EntityManagerInterface $entityManagerInterface): Response
+    {
+        $user = new User();
+        $form = $this->createForm(InscriptionType::class, $user);
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $entityManagerInterface->persist($user);
+            $entityManagerInterface->flush();
+        }
+
+        return $this->render('inscription_form/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
